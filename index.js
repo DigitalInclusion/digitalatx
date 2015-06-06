1299bc95c191c2cafe94343f77be9864b4f57f42
site-demo <site9demo@monumentmail.com>
2015-06-06 15:25:58 -0500

    testing collab with heroku

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Pushing into origin master instead of heroku master!');
+  response.send('Pushing AGAIN into origin master instead of heroku master!');
 });
 
 app.listen(app.get('port'), function() {
