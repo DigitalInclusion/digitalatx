ded64d6d9b824780222994cf9e75d74e755e7982
Alfredo Miranda <alfredo@miranda.io>
2015-06-06 15:53:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('index.html');
+  response.render('index.html');
 });
 
 app.listen(app.get('port'), function() {
