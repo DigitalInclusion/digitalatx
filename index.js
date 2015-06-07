e91db4bebf0bda7cbc4f1c85d3c34a55a8bfe53a
Alfredo Miranda <alfredo@miranda.io>
2015-06-06 21:48:46 -0500

    Add profile navigation functionality

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname,'index.html'));
 });
 
+app.get('/profile1.html', function(request, response) {
+  response.sendFile(path.join(__dirname, 'profile1.html'));
+});
+
+app.get('/profile2.html', function(request, response) {
+  response.sendFile(path.join(__dirname, 'profile2.html'));
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
