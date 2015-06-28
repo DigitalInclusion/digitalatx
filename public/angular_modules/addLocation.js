21a06403a27c9c4e60bbbfb105fd010dd4c8d6fa
unknown <p.v.carroll@gmail.com>
2015-06-28 14:15:19 -0500

    submitting form data to angular ctrl

--- public/angular_modules/addLocation.js
+++ public/angular_modules/addLocation.js
@@ -2,4 +2,11 @@ var addLocationModule = angular.module('addLocationModule', []);
 
 addLocationModule.controller('addLocationCtrl', function($scope) {
 	$scope.testVar = 'CONTROLLER WORKS';
+
+	$scope.formData = {};
+
+	$scope.saveFormData = function(formData) {
+		$scope.formData = angular.copy(formData);
+	};
+	
 });
