bf58ad79c3e52155efa976ddddde5248666b9dd4
unknown <p.v.carroll@gmail.com>
2015-09-26 16:33:57 -0500

    Add Location Form: picture upload working, using fileread directive

--- public/angular_modules/addLocation.js
+++ public/angular_modules/addLocation.js
@@ -8,8 +8,11 @@ addLocationModule.controller('addLocationCtrl', function($scope, $http, FileUplo
 	$scope.programs = [];
 
 	$scope.saveFormData = function(location, programs) {
+		console.log($scope.uploadedPicture);
 		location.programs = programs;
+		location.picture = $scope.uploadedPicture;
 		$scope.formData = angular.copy(location);
+		console.log($scope.formData);
 		$scope.register();
 	};
 
@@ -26,5 +29,23 @@ addLocationModule.controller('addLocationCtrl', function($scope, $http, FileUplo
 	$scope.addProgram = function() {
 		$scope.programs.push({});
 	};
-
 });
+
+addLocationModule.directive("fileread", [function () {
+    return {
+        scope: {
+            fileread: "="
+        },
+        link: function (scope, element, attributes) {
+            element.bind("change", function (changeEvent) {
+                var reader = new FileReader();
+                reader.onload = function (loadEvent) {
+                    scope.$apply(function () {
+                        scope.fileread = loadEvent.target.result;
+                    });
+                }
+                reader.readAsDataURL(changeEvent.target.files[0]);
+            });
+        }
+    }
+}]);
