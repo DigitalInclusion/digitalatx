e69695bf92a22a1b752e7b6f2d167f8a85f4c18c
site-demo <site9demo@monumentmail.com>
2015-10-26 05:35:56 -0500

    new profile page

---
+++ public/angular_modules/profileModule.js
@@ -0,0 +1,48 @@
+var profileModule = angular.module('profileModule', ['ngRoute']);
+
+
+profileModule.config(function ($routeProvider, $locationProvider) {
+	//configure the routing rules here
+    $routeProvider.when('/profile/:name/:siteid', {
+     	controller: 'profileCtrl'
+      });
+
+   $routeProvider.otherwise( {redirectTo: '/'} );
+
+        //routing DOESN'T work without html5Mode
+       $locationProvider.html5Mode(true);
+
+ //    $locationProvider.html5Mode({
+ //  			enabled: true,
+ //  			requireBase: false
+	// });
+
+});
+
+profileModule.controller('profileCtrl', function($scope, $http, $location, $routeParams, $route) {
+
+	var url = $location.$$path;
+	var id = url.substr(-24);
+
+	$http.post('/getLocations', {})
+		.success(function(locations) {
+			console.log("GET LOCATIONS SUCCESS");
+			console.log(locations);
+			$scope.locations = locations;
+			angular.forEach(locations, function(location) {
+
+				console.log("id:",location._id);
+				if (location._id === id ) {
+					console.log("PROFILE MATCH");
+					$scope.profileName = location.name;
+					$scope.profilePicture = location.picture;
+					$scope.profileId = location._id;
+					//console.dir(location);
+				}
+			});
+		}).error(function(err) {
+			console.log("ERROR");
+			console.log(err);
+		}
+	);
+});
