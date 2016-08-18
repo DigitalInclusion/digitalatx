2624e83c379a47715a0a98a708dbbc4d412fedaa
paulcarroll <pvcarroll@gmail.com>
2016-08-17 22:39:55 -0500

    get page tests

--- test/index.js
+++ test/index.js
@@ -1,12 +1,47 @@
 var supertest = require("supertest");
 var app = require("../index");
 
-describe("home", function() {
+describe("get pages", function() {
 
-    var request;
-    it("gets home", function(done) {
-        request = supertest(app)
+    it("gets home page", function(done) {
+        supertest(app)
             .get("/views/index.html")
+            .set("Accept", "text/html")
+            .expect("Content-Type", /html/)
+            .expect(200)
+            .end(done);
+    });
+
+    it("gets locationInventory page", function(done) {
+        supertest(app)
+            .get("/views/locationInventory.html")
+            .set("Accept", "text/html")
+            .expect("Content-Type", /html/)
+            .expect(200)
+            .end(done);
+    });
+
+    it("gets addLocation page", function(done) {
+        supertest(app)
+            .get("/addLocation.html")
+            .set("Accept", "text/html")
+            .expect("Content-Type", /html/)
+            .expect(200)
+            .end(done);
+    });
+
+    it("gets profile page", function(done) {
+        supertest(app)
+            .get("/profile.html")
+            .set("Accept", "text/html")
+            .expect("Content-Type", /html/)
+            .expect(200)
+            .end(done);
+    });
+
+    it("gets storytelling", function(done) {
+        supertest(app)
+            .get("/storytelling.html")
             .set("Accept", "text/html")
             .expect("Content-Type", /html/)
             .expect(200)
