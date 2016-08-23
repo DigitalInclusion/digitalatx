16b585f50c1224efc2515c5d87ce3b0292ffb2b0
paulcarroll <pvcarroll@gmail.com>
2016-08-23 17:03:28 -0500

    comment out session secret to fix production

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(multer());
 app.use(session({
-  secret: require("./config/sessionSecret.js"),
+  // secret: require("./config/sessionSecret.js"),
   resave: true,
   saveUninitialized: true
 }));
